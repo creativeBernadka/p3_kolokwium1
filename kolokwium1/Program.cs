7bc9e64ea689d9f5e1aebe94dc9cb3712d762640
creativeBernadka <bfryczkowska@gmail.com>
2019-11-22 08:44:54 +0100

    ADD: zad1 added

--- kolokwium1/Program.cs
+++ kolokwium1/Program.cs
@@ -4,9 +4,24 @@ namespace kolokwium1
 {
     class Program
     {
+
+        static double Zad1(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return 1.5 * Zad1(n - 1) - 1;
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ZADANIE 1 - ciag arytmetyczny");
+            Console.WriteLine("Podaj numer wyrazu ciagu ktorego wartosc chcesz poznac:");
+            string n = Console.ReadLine();
+            double result1 = Zad1(Int32.Parse(n));
+            Console.WriteLine(n + "-ty wyraz ciagu ma wartosc " + result1);
         }
     }
 }
