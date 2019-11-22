2af1b515c03eea30534e45eff5dd6b33a8fd98bb
creativeBernadka <bfryczkowska@gmail.com>
2019-11-22 08:29:43 +0100

    initial commit

---
+++ kolokwium1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kolokwium1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
